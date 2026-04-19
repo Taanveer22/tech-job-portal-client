082e9d3486613531932699f1301070a5f7db871d
tanvir-wd22 <taanveer469@gmail.com>
2026-04-20 00:32:48 +0600

    alhamdulliah context setup done and register and sign in done

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,45 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
+import auth from "../utilities/firebase.config";
+import AuthContext from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signInUser,
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,28 @@
 import Lottie from "lottie-react";
+import { useContext } from "react";
 import registerLottie from "../assets/registerLottie.json";
+import AuthContext from "../context/AuthContext";
 
 const Register = () => {
+  const { createUser } = useContext(AuthContext);
+
+  const handleRegisterForm = (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const photo = e.target.photo.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, photo, email, password);
+
+    createUser(email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,18 +30,39 @@ const Register = () => {
           <Lottie animationData={registerLottie}></Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
+          <form onSubmit={handleRegisterForm} className="card-body">
             <fieldset className="fieldset">
+              <label className="label">Name</label>
+              <input
+                name="name"
+                type="text"
+                className="input"
+                placeholder="Name"
+              />
+              <label className="label">Photo</label>
+              <input
+                name="photo"
+                type="text"
+                className="input"
+                placeholder="Photo"
+              />
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                name="email"
+                type="email"
+                className="input"
+                placeholder="Email"
+              />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4">Login</button>
+              <input
+                name="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+              />
+              <button className="btn btn-neutral mt-4">Register</button>
             </fieldset>
-          </div>
+          </form>
         </div>
       </div>
     </div>
