bc1f95db1ba935dc9a7b1bfca729940ef109b8cc
Taanveer22 <taanveer469@gmail.com>
2026-04-24 12:37:04 +0600

    alhamdulliah aggregated data showed in the my applications component

--- src/pages/MyApplications.jsx
+++ src/pages/MyApplications.jsx
@@ -14,7 +14,61 @@ const MyApplications = () => {
   }, [user?.email]);
   return (
     <div>
-      <h1>{apps.length}</h1>
+      <h1 className="text-2xl font-semibold text-center mb-4">
+        My Applications Total : {apps.length}
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Company Name</th>
+              <th>Job Title</th>
+              <th>Job Type</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {apps.map((appItem) => (
+              <tr key={appItem._id}>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={appItem?.company_logo}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{appItem?.company}</div>
+                      <div className="text-sm opacity-50">
+                        {appItem?.location}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>{appItem?.title}</td>
+                <td>{appItem?.jobType}</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">Delete</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
